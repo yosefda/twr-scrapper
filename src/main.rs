b0e596d828f0bb94e605b7d1a5626400e5dd015f
Yosef da Iry <yosefda@gmail.com>
2018-03-26 21:58:38 +1100

    naive implementation completed

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,42 @@
 extern crate reqwest;
+extern crate select;
 
 use std::io::Read;
+use select::document::Document;
+use select::predicate::{Predicate, Attr, Class, Name, Element};
 
 const TWR_ARCHIVE_URL: &str = "https://this-week-in-rust.org/blog/archives/index.html";
 
 fn main() {
-    let html = download_url(TWR_ARCHIVE_URL);
-    println!("{}", html);
+    // download archive page
+    let archive_page = reqwest::get(TWR_ARCHIVE_URL)
+        .unwrap()
+        .text()
+        .unwrap();
+
+    // get list of issues and crawl them
+    let archive_doc = Document::from(archive_page.as_str());
+    for issue_node in archive_doc.find(Class("col-sm-8").descendant(Name("a"))) {
+        println!("{}", issue_node.text());
+        let issue_url = issue_node.attr("href").unwrap();
+        let issue_page = reqwest::get(issue_url)
+            .unwrap()
+            .text()
+            .unwrap();
+        let issue_doc = Document::from(issue_page.as_str());
+
+        // only crawl issue with News & Blog Posts section
+        for news_blog_posts in issue_doc.find(Name("ul")).take(1) {
+            for post in news_blog_posts.children() {
+                match post.first_child() {
+                    Some(_) => if post.first_child().unwrap().is(Element) {
+                        let link = post.first_child().unwrap();
+                        println!("\t{} --> {}", link.text(), link.attr("href").unwrap());
+                    },
+                    _ => print!(""),
+                }
+            }
+        }
+    }
 }
 
-fn download_url(url: &str) -> String {
-    let mut response = reqwest::get(url).expect("Failed to send request");
-
-    let mut buf = String::new();
-    response.read_to_string(&mut buf).expect("Failed to read response");
-
-    buf
-}
