41d6b33b1774ae1dfecbbf0618c17971bc428129
Yosef da Iry <yosefda@gmail.com>
2018-03-29 04:03:04 +1100

    refactored into download_url() and get_issues()

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,27 @@ use select::predicate::{Predicate, Attr, Class, Name};
 
 const TWR_ARCHIVE_URL: &str = "https://this-week-in-rust.org/blog/archives/index.html";
 
+#[derive(Debug)]
+struct Issue {
+    title: String,
+    url: String,
+}
+
+#[derive(Debug)]
+struct Article {
+    title: String,
+    url: String,
+}
+
 fn main() {
-    // download archive page
-    let archive_page = reqwest::get(TWR_ARCHIVE_URL)
-        .unwrap()
-        .text()
-        .unwrap();
+    let issues = get_issues(TWR_ARCHIVE_URL);
 
-    // get list of issues and crawl them
-    let archive_doc = Document::from(archive_page.as_str());
-    for issue_node in archive_doc.find(Class("col-sm-8").descendant(Name("a"))) {
-        let issue_url = issue_node.attr("href").unwrap();
-        let issue_page = reqwest::get(issue_url)
-            .unwrap()
-            .text()
-            .unwrap();
+    for issue in issues {
+        println!("{}", issue.title);
+
+        // @todo refactor these lines using get_articles()
+        let issue_page = download_url(issue.url.as_str());
         let issue_doc = Document::from(issue_page.as_str());
-
-        println!("{}", issue_node.text());
 
         // only crawl issue with News and Blog Posts section
         // @todo add ability to add more sections e.g. notable links
@@ -41,5 +44,42 @@ fn main() {
         }
         print!("\n");
     }
+}
+
+fn download_url(url: &str) -> String {
+    reqwest::get(url)
+        .unwrap()
+        .text()
+        .unwrap()
+}
+
+
+fn get_issues(archive_url: &str) -> Vec<Issue> {
+    let mut issues = Vec::new();
+
+    // parse archive page to get urls of previous issues
+    let archive_page = download_url(TWR_ARCHIVE_URL);
+    let archive_doc = Document::from(archive_page.as_str());
+    for issue_node in archive_doc.find(Class("col-sm-8").descendant(Name("a"))) {
+        issues.push(Issue {
+            title: issue_node.text(),
+            url: issue_node.attr("href").unwrap().to_owned(),
+        });
+    }
+
+    issues
+}
+
+// @todo finish this function
+fn get_articles(issue_url: &str) -> Vec<Article> {
+    let mut article_entries = Vec::new();
+
+    // parse page to get list of article entries
+    let issue_page = download_url(issue_url);
+    let issue_doc = Document::from(issue_page.as_str());
+
+    // @todo put parsing of issue doc in separate function
+
+    article_entries
 }
 
